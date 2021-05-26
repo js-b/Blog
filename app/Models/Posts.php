10d39d71cb4a79de8231cb625608c7ca6a074e39
Jaxan <whitedragon99@gk.ru>
2021-05-26 15:23:35 +0500

    В модели Posts добавил функцию для получения данных из БД

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use DB;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Posts extends Model
 {
     use HasFactory;
+    public function PostModel()
+    {
+        return DB::table('posts')->select('*')->get();
+    }
 }
